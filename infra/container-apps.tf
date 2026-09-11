f95ed19c31f5ce2785b1bab94f8c798be99ff50c
neil <64820853+networkneil@users.noreply.github.com>
2026-09-11 15:05:29 -0700

    fix: add back Log Analytics Workspace required by Container Apps

--- infra/container-apps.tf
+++ infra/container-apps.tf
@@ -1,8 +1,21 @@
+# Log Analytics Workspace (required by Container Apps)
+resource "azurerm_log_analytics_workspace" "neko" {
+  name                = "${var.environment_name}-logs"
+  resource_group_name = azurerm_resource_group.neko.name
+  location            = azurerm_resource_group.neko.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = var.tags
+}
+
 # Container Apps Environment
 resource "azurerm_container_app_environment" "neko" {
   name                       = "${var.environment_name}-env"
   resource_group_name        = azurerm_resource_group.neko.name
   location                   = azurerm_resource_group.neko.location
+  logs_destination           = "log-analytics"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.neko.id
 
   tags = var.tags
 }
